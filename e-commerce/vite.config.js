10297381237415b7f98dc6fb67c8d6d71c7a906c
Elijah Alexander <elijahkylealexander@gmail.com>
2025-04-27 06:02:39 +0100

    bla

--- e-commerce/vite.config.js
+++ e-commerce/vite.config.js
@@ -1,15 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  root: './', // Ensure Vite uses the correct root folder
-  build: {
-    outDir: 'dist', // Explicit output directory
-    rollupOptions: {
-      input: './index.html', // Ensure Vite reads index.html from the root directory
-    },
-  },
-});
+  server: {port : 5174}
+})
